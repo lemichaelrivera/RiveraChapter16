0575989284057397ddf84085e358b3450481d812
lemichaelrivera <151088475+lemichaelrivera@users.noreply.github.com>
2023-12-05 15:07:51 -0700

    Delete RiveraChapter16 directory

--- RiveraChapter16/data-raw/FlagWeatherStation.R
+++
@@ -1,9 +0,0 @@
-# Read in the data.  Do some cleaning/verification
-library(tidyverse)
-FlagWeatherStation <- read.csv('data-raw/Pulliam_Airport_Weather_Station.csv') %>%
-  select(DATE, PRCP, SNOW, TMAX, TMIN) %>%
-  mutate(DATE2 = lubridate::ymd(DATE)) %>%
-  rename('chr_date' = DATE)
-
-usethis::use_data(FlagWeatherStation, overwrite = TRUE)
-
--- RiveraChapter16/data-raw/FlagWeatherStationCleaner.R
+++
@@ -1,6 +0,0 @@
-#call resulting data frame 'FlagWeatherStation' and
-#save a .r file to the data/ directory using the command
-#usethis::use_data(FlagstaffWeatherStation)
-
-usethis::use_data(FlagWeatherStation, overwrite = TRUE)
-
