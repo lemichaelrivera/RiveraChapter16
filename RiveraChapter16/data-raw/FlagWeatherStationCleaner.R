912ff867520671f92a1391ab8f841cf5b76a0781
lemichaelrivera <151088475+lemichaelrivera@users.noreply.github.com>
2023-12-05 15:06:40 -0700

    Add files via upload

---
+++ RiveraChapter16/data-raw/FlagWeatherStationCleaner.R
@@ -0,0 +1,6 @@
+#call resulting data frame 'FlagWeatherStation' and
+#save a .r file to the data/ directory using the command
+#usethis::use_data(FlagstaffWeatherStation)
+
+usethis::use_data(FlagWeatherStation, overwrite = TRUE)
+
---
+++ RiveraChapter16/docs/FizzBuzzDoc.R
@@ -0,0 +1,18 @@
+#' Documenting the FizzBuzz function
+#'
+#' The goal is to count as high as you can, but for
+#' any number evenly divisible by 3, substitute “Fizz”
+#' and any number evenly divisible by 5, substitute
+#' “Buzz,” and if it is divisible by both, substitute
+#' “Fizz Buzz.”
+#' So the sequence will look like 1, 2, Fizz, 4,
+#' Buzz, Fizz, 7, 8, Fizz, ...
+#'
+#' @param runtime is a real integer
+#' @return a list of the following: a number, 'Fizz', 'Buzz', or 'FizzBuzz'
+#' @examples
+#' FizzBuzz(20)
+#' FizzBuzz(20:50)
+#' @export
+#'
+
